0a9e396b676eb00885e96c838dcc686332819a4b
elboroo <bogus@bogus-laptop.(none)>
2009-11-26 22:34:37 +0100

    tagowanie v1

--- config/initializers/gravatar_defaults.rb
+++ config/initializers/gravatar_defaults.rb
@@ -1,2 +1,2 @@
-ActionView::Base.default_gravatar_image  = "http://localhost:3000/images/default_avatar.png"
+ActionView::Base.default_gravatar_image  = "http://elcontacts.heroku.com/images/default_avatar.png"
 ActionView::Base.default_gravatar_size  = 30
---
+++ config/initializers/gravatar_defaults.rb~
@@ -0,0 +1,2 @@
+ActionView::Base.default_gravatar_image  = "http://localhost:3000/images/default_avatar.png"
+ActionView::Base.default_gravatar_size  = 30
